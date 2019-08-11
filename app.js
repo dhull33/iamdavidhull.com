b13704f01e40c41d3f24618d692d2acc30a124aa
David Hull <david.hull37@gmail.com>
2019-08-11 12:51:28 -0500

    Added express-sslify to force use of https

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const enforce = require('express-sslify');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Enforces HTTPS
+app.use(enforce.HTTPS({ trustProtoHeader: true })); // eslint-disable-line new-cap
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
