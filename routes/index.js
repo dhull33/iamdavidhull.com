24e2167711cdfeb792f38c69d352428392701190
David Hull <david.hull37@gmail.com>
2019-08-24 16:14:22 -0500

    Added a new logo and background

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const app = express();
 
 /* GET home page. */
 app.get('/', (req, res, next) => {
-  res.render('index', { title: 'David Hull | Node.js Engineer' });
+  return res.render('index', {
+    title: 'David Hull | Node.js Engineer',
+    srcset:
+      '/images/bg-1456x1600.jpeg 1456w, /images/bg-910x1000.jpeg 910w,' +
+      ' /images/bg-728x800.jpeg 728w, /images/bg-455x500.jpeg 455w'
+  });
 });
 
 module.exports = app;
