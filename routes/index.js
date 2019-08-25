2bafbd92296e0296dc459043089048e8f5c3664f
David Hull <david.hull37@gmail.com>
2019-08-25 16:50:53 -0500

    Re-added more pictures

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const app = express();
 app.get('/', (req, res, next) => {
   return res.render('index', {
     title: 'David Hull | Node.js Engineer',
-    srcset:
-      '/images/brain-1024x1024.png 1024w,' +
-      ' /images/brain-800x800.png 800w, /images/brain-500x500.png 500w',
+    srcset: ' /images/brain-800x800.png 800w, /images/brain-500x500.png 500w',
     altText: 'David Hull - Node.js & JavaScript Developer'
   });
 });
