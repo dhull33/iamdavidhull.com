fc3fb0dd2b7e1ed0081b93f04bea223c29105b53
David Hull <david.hull37@gmail.com>
2019-08-26 15:20:21 -0500

    Fixed navbar issue

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const app = express();
 app.get('/', (req, res, next) => {
   return res.render('index', {
     title: 'David Hull | Node.js Engineer',
-    srcset: ' /images/brain-800x800.png 800w, /images/brain-500x500.png 500w',
+    srcset:
+      ' /images/brain-800x800.png 800w, /images/brain-500x500.png' +
+      ' 500w, /images/brain-260x260.png 260w',
     altText: 'David Hull - Node.js & JavaScript Developer'
   });
 });
