a9a48c99ddf4daeed67cb7e7d9e4c00f1d92bdaf
David Hull <david.hull37@gmail.com>
2019-08-30 12:48:39 -0500

    Added down arrow and function that makes it disappear when clicked

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,15 @@
+// Toggle Navbar
 const mainNav = document.getElementById('js-menu');
 
 const navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', () => {
   return mainNav.classList.toggle('active');
+});
+
+// Arrow Down on Mobile Screen
+const downArrow = document.getElementById('down-arrow');
+
+downArrow.addEventListener('click', () => {
+  return downArrow.classList.toggle('not-active');
 });
