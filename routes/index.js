472eb4aac256b1a748cbad02dbcffced2b7cc6a3
David Hull <david.hull37@gmail.com>
2019-09-07 16:16:24 -0500

    Removed some comments

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ const app = express();
 app.get('/', (req, res, next) => {
   return res.render('index', {
     title: 'David Hull | Node.js Engineer',
-    srcset:
-      '/images/free-1600x1015.png 1600w, /images/free-1000x630.png 1000w, /images/free-800x504.png 800w',
     altText: 'David Hull - Node.js & JavaScript Developer'
   });
 });
