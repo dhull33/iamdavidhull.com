ceb8f804ecc191f3ff69ca81524c705cc2802149
David Hull <david.hull37@gmail.com>
2019-09-07 16:13:32 -0500

    Made the images more responsive

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,7 @@ app.get('/', (req, res, next) => {
   return res.render('index', {
     title: 'David Hull | Node.js Engineer',
     srcset:
-      ' /images/free-1000x630.png 1000w, /images/free-800x504.png' +
-      ' 500w, /images/free-500x315.png 260w',
+      '/images/free-1600x1015.png 1600w, /images/free-1000x630.png 1000w, /images/free-800x504.png 800w',
     altText: 'David Hull - Node.js & JavaScript Developer'
   });
 });
