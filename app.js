35ba936d203b00cf96b6e7ad5bdd107a31f517ae
David Hull <david.hull37@gmail.com>
2019-09-26 14:46:12 -0500

    I forgot to remove the require statement for express-sslify. It is now removed.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const enforce = require('express-sslify');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
